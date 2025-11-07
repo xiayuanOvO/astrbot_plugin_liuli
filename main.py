c99a8226345c96e147ee4fd8abbc457cd2ae7844
xiayuan <xiayuanovo@163.com>
2025-11-07 14:58:03 +0800

    fiexd

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from config import database
 
 @register("琉璃", "夏源", "来自异世界的娱乐插件", "0.0.1")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-        import config.database
-        config.database.init_db()
+        database.init_db()
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
     
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("注册")
+    @filter.command("hi")
     async def reg(self, event: AstrMessageEvent):
         """玩家注册""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         user_name = event.get_sender_name()

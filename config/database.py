9e3c060b0173f6b1c07a0b9a841325e285940aec
xiayuan <xiayuanovo@163.com>
2025-11-07 15:31:05 +0800

    fix(database):修复数据库初始化方法调用

--- config/database.py
+++ config/database.py
@@ -1,12 +1,13 @@
 from tortoise import Tortoise
 
+
 async def init_db():
     """
     初始化数据库连接
     1
     """
-    await Tortoise.init_db(
-        "sqlite://liuli.db",
+    await Tortoise.init(
+        db_url="sqlite://liuli.db",
         modules={"models": ["models"]},
     )
     await Tortoise.generate_schemas()

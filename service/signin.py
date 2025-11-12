e603d7725aee12ee9fedb08371bdacd3fefbcd05
xiayuan <xiayuanovo@163.com>
2025-11-12 11:33:23 +0800

    feat(config): 实现插件配置管理功能

--- service/signin.py
+++ service/signin.py
@@ -1,7 +1,7 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 
 
-@filter.command("hi")
+@filter.command("ii")
 async def signin(event: AstrMessageEvent):
     print(event)
     yield event.plain_result("签到成功")
